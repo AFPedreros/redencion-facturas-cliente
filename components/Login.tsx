a341f7f002fb5ceffd821e91c20d2b96a2ac4184
AFPedreros <felipe.pedreros94@gmail.com>
2023-02-20 09:05:34 -0500

    commit

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,8 +2,39 @@
 import { ReceiptPercentIcon } from '@heroicons/react/24/outline';
 import { DocumentTextIcon } from '@heroicons/react/24/outline';
 import { TicketIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 
 export default function Login() {
+	const [form, setForm] = useState({
+		email: '',
+		password: '',
+	});
+
+	function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+		const { value, name } = e.target;
+		setForm((prevState) => {
+			return {
+				...prevState,
+				[name]: value,
+			};
+		});
+	}
+
+	function handleOnSubmit(e: React.MouseEvent<HTMLButtonElement>) {
+		e.preventDefault();
+
+		if (!form.email) {
+			alert('Please enter your email.');
+			return false;
+		} else if (!form.password) {
+			alert('Please enter your password.');
+			return false;
+		}
+
+		console.log(form.email);
+		console.log(form.password);
+	}
+
 	return (
 		<div className="bg-white md:flex">
 			<div className="flex flex-col items-center justify-center h-screen p-8 md:border-r md:border-black md:w-1/3">
@@ -29,21 +60,25 @@ export default function Login() {
 					</div>
 				</div>
 			</div>
-			<div className="flex flex-col justify-center h-screen p-8 mx-auto md:w-2/3">
+			<form className="flex flex-col justify-center h-screen p-8 mx-auto md:w-2/3">
 				<h2 className="mb-12 text-2xl font-light text-center">Iniciar sesión</h2>
 				<input
+					value={form.email}
+					onChange={handleChange}
 					type="email"
-					id="email"
 					className="bg-gray-50 md:max-w-md md:mx-auto border mb-4 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					placeholder="Correo Electrónico"
 					required
+					name="email"
 				/>
 				<input
+					value={form.password}
+					onChange={handleChange}
 					type="password"
-					id="password"
 					className="bg-gray-50 mb-12 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full md:max-w-md md:mx-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					placeholder="•••••••••"
 					required
+					name="password"
 				/>
 				<p className="mx-auto mb-2 text-[#707070] text-sm">Olvidé mi contraseña</p>
 				<div className="flex gap-2 mx-auto text-sm h-fit">
@@ -54,11 +89,12 @@ export default function Login() {
 				</div>
 				<button
 					type="button"
+					onClick={handleOnSubmit}
 					className="mt-8 md:w-fit focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-12 md:mx-auto py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900"
 				>
 					Ingresar
 				</button>
-			</div>
+			</form>
 		</div>
 	);
 }
