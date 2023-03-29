fd9ab1b763513b9f5019be44fe32ff0cfbdbc082
AFPedreros <felipe.pedreros94@gmail.com>
2023-03-29 10:52:31 -0500

    commit

--- components/ReceiptTable.tsx
+++ components/ReceiptTable.tsx
@@ -5,6 +5,7 @@ import { db } from '../firebase';
 // Importa el hook personalizado useAuth
 import { useAuth } from '../context/AuthContext';
 import { useEffect, useState } from 'react';
+import AddReceipt from './AddReceipt';
 
 // Crea un tipo Receipt para los objetos de factura.
 type Props = { receiptsData: any };
@@ -15,7 +16,6 @@ export default function ReceiptTable({ receiptsData }: Props) {
 	// Usa el hook useAuth para obtener el usuario
 	const { user } = useAuth();
 
-	const [showModal, setShowModal] = useState(false);
 	const [modalReceiptId, setModalReceiptId] = useState(null);
 
 	useEffect(() => {
@@ -91,17 +91,12 @@ export default function ReceiptTable({ receiptsData }: Props) {
 		} else {
 			setModalReceiptId(receiptId); // open the modal for the clicked receipt
 		}
-		setShowModal(!showModal);
 	};
-
-	function hideModal() {
-		setShowModal(false);
-	}
 
 	return (
 		<>
 			{!receiptsData?.empty ? (
-				<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+				<div className="overflow-x-auto shadow-md sm:rounded-lg">
 					<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
 						<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
 							<tr>
@@ -137,63 +132,33 @@ export default function ReceiptTable({ receiptsData }: Props) {
 												Ver
 											</a>
 										</td>
-										<td className="flex items-center px-6 py-4 space-x-3">
+										<td className="flex items-center px-6 py-4">
 											<button onClick={() => toggleModal(rec.id)} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
 												Editar
 											</button>
-											{showModal && modalReceiptId === rec.id && (
+											{modalReceiptId === rec.id && (
 												<div
 													data-modal-backdrop="static"
 													tabIndex={-1}
 													aria-hidden="true"
-													className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full"
+													className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center w-full h-full p-4 m-0 overflow-hidden"
 												>
-													<div className="relative w-full h-full max-w-2xl md:h-auto">
-														<div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-															<div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-																<h3 className="text-xl font-semibold text-gray-900 dark:text-white">{rec.id}</h3>
-																<button
-																	type="button"
-																	className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-																	onClick={hideModal}
-																>
-																	<svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-																		<path
-																			fillRule="evenodd"
-																			d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-																			clipRule="evenodd"
-																		></path>
-																	</svg>
-																</button>
-															</div>
-
-															<div className="p-6 space-y-6">
-																<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-																	With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world
-																	are updating their terms of service agreements to comply.
-																</p>
-																<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-																	The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of
-																	data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that
-																	could personally affect them.
-																</p>
-															</div>
-															<div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-																<button
-																	onClick={toggleModal}
-																	type="button"
-																	className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-																>
-																	I accept
-																</button>
-																<button
-																	onClick={toggleModal}
-																	type="button"
-																	className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-																>
-																	Decline
-																</button>
-															</div>
+													<div className="w-full max-w-2xl bg-white rounded-lg shadow-md">
+														<div className="flex items-start justify-between p-4 border-b">
+															<h3 className="text-lg font-semibold text-gray-900">{rec.id}</h3>
+															<button type="button" className="text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700" onClick={toggleModal}>
+																<span className="sr-only">Close</span>
+																<svg className="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+																	<path
+																		fillRule="evenodd"
+																		d="M4.293 4.293a1 1 0 011.414 0L12 10.586l6.293-6.293a1 1 0 111.414 1.414L13.414 12l6.293 6.293a1 1 0 01-1.414 1.414L12 13.414l-6.293 6.293a1 1 0 01-1.414-1.414L10.586 12 4.293 5.707a1 1 0 010-1.414z"
+																		clipRule="evenodd"
+																	/>
+																</svg>
+															</button>
+														</div>
+														<div className="p-4">
+															<AddReceipt />
 														</div>
 													</div>
 												</div>
@@ -211,7 +176,7 @@ export default function ReceiptTable({ receiptsData }: Props) {
 														console.log(e);
 													}
 												}}
-												className="font-medium text-red-600 hover:underline"
+												className="ml-2 font-medium text-red-600 hover:underline"
 											>
 												Borrar
 											</button>
@@ -330,7 +295,7 @@ export default function ReceiptTable({ receiptsData }: Props) {
 										</svg>
 									</label>
 								</td>
-								<td className="flex items-center px-6 py-4 space-x-3">
+								<td className="flex items-center px-6 py-4">
 									<div>
 										<svg
 											aria-hidden="true"
