28202c2c0bc7c85e1ac1f577df0b3daa6a4eda34
AFPedreros <felipe.pedreros94@gmail.com>
2023-03-29 09:04:13 -0500

    commit

--- components/ReceiptTable.tsx
+++ components/ReceiptTable.tsx
@@ -15,6 +15,9 @@ export default function ReceiptTable({ receiptsData }: Props) {
 	// Usa el hook useAuth para obtener el usuario
 	const { user } = useAuth();
 
+	const [showModal, setShowModal] = useState(false);
+	const [modalReceiptId, setModalReceiptId] = useState(null);
+
 	useEffect(() => {
 		// Si hay facturas, crear un array de objetos factura a partir de los datos obtenidos de Firebase.
 		if (receiptsData) {
@@ -32,7 +35,6 @@ export default function ReceiptTable({ receiptsData }: Props) {
 
 			// Ordena el array de facturas por fecha de registro y asigna el array ordenado a la variable facturas.
 			const sortedReceipt = sortByRegistrationDate(receiptsArray);
-			console.log(sortedReceipt);
 			setReceipts(sortedReceipt);
 		}
 	}, [receiptsData]);
@@ -83,6 +85,19 @@ export default function ReceiptTable({ receiptsData }: Props) {
 		setReceipts(updatedReceipts);
 	}
 
+	const toggleModal = (receiptId: any) => {
+		if (modalReceiptId === receiptId) {
+			setModalReceiptId(null); // close the modal if it's already open for this receipt
+		} else {
+			setModalReceiptId(receiptId); // open the modal for the clicked receipt
+		}
+		setShowModal(!showModal);
+	};
+
+	function hideModal() {
+		setShowModal(false);
+	}
+
 	return (
 		<>
 			{!receiptsData?.empty ? (
@@ -123,9 +138,66 @@ export default function ReceiptTable({ receiptsData }: Props) {
 											</a>
 										</td>
 										<td className="flex items-center px-6 py-4 space-x-3">
-											<a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+											<button onClick={() => toggleModal(rec.id)} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
 												Editar
-											</a>
+											</button>
+											{showModal && modalReceiptId === rec.id && (
+												<div
+													data-modal-backdrop="static"
+													tabIndex={-1}
+													aria-hidden="true"
+													className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full"
+												>
+													<div className="relative w-full h-full max-w-2xl md:h-auto">
+														<div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+															<div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+																<h3 className="text-xl font-semibold text-gray-900 dark:text-white">{rec.id}</h3>
+																<button
+																	type="button"
+																	className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+																	onClick={hideModal}
+																>
+																	<svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+																		<path
+																			fillRule="evenodd"
+																			d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+																			clipRule="evenodd"
+																		></path>
+																	</svg>
+																</button>
+															</div>
+
+															<div className="p-6 space-y-6">
+																<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+																	With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world
+																	are updating their terms of service agreements to comply.
+																</p>
+																<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+																	The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of
+																	data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that
+																	could personally affect them.
+																</p>
+															</div>
+															<div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+																<button
+																	onClick={toggleModal}
+																	type="button"
+																	className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+																>
+																	I accept
+																</button>
+																<button
+																	onClick={toggleModal}
+																	type="button"
+																	className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+																>
+																	Decline
+																</button>
+															</div>
+														</div>
+													</div>
+												</div>
+											)}
 											<button
 												onClick={async () => {
 													// Se accede al almacenamiento de Firebase y se establece la referencia donde se almacenará el archivo.
