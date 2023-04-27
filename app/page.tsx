1d215e8e470da232f4438d66788f20305ab5aa11
AFPedreros <felipe.pedreros94@gmail.com>
2023-04-27 17:26:16 -0500

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,15 @@
 'use client';
-// Importa el componente Login desde el directorio ../components
 import Login from '@/components/Login';
-// Importa el hook useRouter de Next.js
-import { useRouter, redirect } from 'next/navigation';
-// Importa el hook personalizado useAuth
+import { redirect } from 'next/navigation';
 import { useAuth } from '@/context/AuthContext';
 
-// Define el objeto de rutas como una constante
 const routes = {
 	receipts: '/facturas',
 };
 
 function page() {
-	// Usa el hook useAuth para obtener el objeto de usuario
 	const { user } = useAuth();
-	// Usa el hook useRouter para obtener acceso al router de Next.js
-	const router = useRouter();
 
-	// Redirige al usuario a la página de facturas si ya ha iniciado sesión
 	if (user) {
 		redirect(routes.receipts);
 	}
