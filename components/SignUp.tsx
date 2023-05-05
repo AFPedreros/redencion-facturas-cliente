54b99a9f58f68f6f3e32354a7f8962d128b0e642
AFPedreros <felipe.pedreros94@gmail.com>
2023-05-05 09:41:04 -0500

    commit

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -168,7 +168,7 @@ export default function SignUp() {
 				required
 				name="cellphone"
 			/> */}
-			<Input id="cellphone" ref={(el) => (formRef.current.cellphone = el)} placeholder="Número de documento" type="tel" onKeyDown={handleKeyDown} required />
+			<Input id="cellphone" ref={(el) => (formRef.current.cellphone = el)} placeholder="Número de celular" type="tel" onKeyDown={handleKeyDown} required />
 			{/* <input
 				className="bg-gray-50 md:w-full border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 				value={form.email}
